c802de186489e1552534c1848907f227ae25346f
gayuabraham <gayuabraham@gmail.com>
2026-03-12 23:05:41 +0530

    fix sidebar path

--- src/Component/CategoryPage/CategoryPage.jsx
+++ src/Component/CategoryPage/CategoryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import Sidebar from "../Sidebar/SideBar";
+import Sidebar from "../SideBar/SideBar";
 import { StoreContext } from "../../Context/StoreContext";
 import { FaEye, FaRegHeart, FaShoppingCart } from "react-icons/fa";
 import "./CategoryPage.css";
